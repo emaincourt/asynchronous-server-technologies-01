a2ad47eea79ffc8400ca8da3b0dc191a018b4ae3
Unknown <e.maincourt@gmail.com>
2017-10-22 21:06:26 +0200

    Trying to fix issues with Coffee

--- bin/test.sh
+++ bin/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-./node_modules/.bin/coffee -c -o lib/ dist/ \
+yarn build \
   && ./node_modules/.bin/babel tests -d tmp \
   && ./node_modules/.bin/replace-in-file "../lib" "../dist" tmp/* \
   && ./node_modules/.bin/mocha tmp/* && rm -rf tmp

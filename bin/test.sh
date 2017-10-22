ca9fc1f10d2e63beb4fa6d3cf945939ebad2807f
Unknown <e.maincourt@gmail.com>
2017-10-22 21:02:08 +0200

    Updated testing script for being able to use Babel and Coffee together

--- bin/test.sh
+++ bin/test.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-node_modules/.bin/mocha --require babel-register tests/*
+./node_modules/.bin/coffee -c -o lib/ dist/ \
+  && ./node_modules/.bin/babel tests -d tmp \
+  && ./node_modules/.bin/replace-in-file "../lib" "../dist" tmp/* \
+  && ./node_modules/.bin/mocha tmp/* && rm -rf tmp
